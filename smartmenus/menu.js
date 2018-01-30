663e6321674d60cf9294a324b7d61d6759d6ed21
K MacLeod <ksmacleod99@gmail.com>
2018-01-29 19:07:54 -0500

    first commit

---
+++ smartmenus/menu.js
@@ -0,0 +1,38 @@
+
+// SmartMenus mobile menu toggle button
+$(function() {
+  var $mainMenuState = $('#main-menu-state');
+  if ($mainMenuState.length) {
+    // animate mobile menu
+    $mainMenuState.change(function(e) {
+      var $menu = $('#main-menu');
+      if (this.checked) {
+        $menu.hide().slideDown(250, function() { $menu.css('display', ''); });
+      } else {
+        $menu.show().slideUp(250, function() { $menu.css('display', ''); });
+      }
+    });
+    // hide mobile menu beforeunload
+    $(window).bind('beforeunload unload', function() {
+      if ($mainMenuState[0].checked) {
+        $mainMenuState[0].click();
+      }
+    });
+  };
+});
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".main-nav").css({'background':'url(images/tile1.png)', 'background-repeat':'repeat-x', 'background-color':'#f69a58'});
+       } else {
+          $('.main-nav').css('background-color', 'transparent');
+       }
+   });
+    }
+});
